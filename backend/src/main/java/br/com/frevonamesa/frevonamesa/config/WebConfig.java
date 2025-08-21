2b990617fa65b7bb5f952388cd1c20a101872834
Isaías Roberto <76740216+anidio@users.noreply.github.com>
2025-08-20 22:34:22 -0300

    alterar cors

--- backend/src/main/java/br/com/frevonamesa/frevonamesa/config/WebConfig.java
+++ backend/src/main/java/br/com/frevonamesa/frevonamesa/config/WebConfig.java
@@ -1,5 +1,6 @@
 package br.com.frevonamesa.frevonamesa.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Aplica a todos os endpoints que começam com /api
-                .allowedOrigins("http://localhost:5173") // Permite requisições desta origem
+                .allowedOrigins(allowedOrigins) // Permite requisições destas origens
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // Permite todos estes métodos
                 .allowedHeaders("*") // Permite todos os cabeçalhos
                 .allowCredentials(true); // Permite o envio de credenciais (cookies, etc.)
